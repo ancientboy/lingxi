bb8b30f9df57b8e86d23b6a182a392493f037b18
古风 <gufeng@lingxi.ai>
2026-03-02 18:19:18 +0800

    fix: 增加 Express body parser 限制到 10mb

--- lingxi-cloud/backend/index.js
+++ lingxi-cloud/backend/index.js
@@ -29,7 +29,7 @@ expressWs(app);
 
 // 中间件
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 
 // 托管前端静态文件（禁用 HTML 缓存）
 app.use(express.static(join(__dirname, '../frontend'), {
